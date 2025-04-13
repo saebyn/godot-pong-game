35d84903d11c9456137be7b511078e18b8b5db97
John Weaver <john@saebyn.info>
2025-04-13 15:57:03 -0700

    Add keybindings button functionality and update pause menu exit button text

--- menu.gd
+++ menu.gd
@@ -2,6 +2,13 @@ extends Control
 
 signal restart_game
 
+@onready var keybindings_scene: PackedScene = preload("res://keybindings/keybindings.tscn")
+
+
+func _on_return_main_menu_button_pressed() -> void:
+  # Unpause the game
+  get_tree().paused = false
+  get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _on_exit_button_pressed() -> void:
   get_tree().quit()
@@ -21,3 +28,8 @@ func _on_resume_button_pressed() -> void:
   get_tree().paused = false
   # Hide the pause menu
   hide()
+
+
+func _on_keybindings_button_pressed() -> void:
+  var keybindings = keybindings_scene.instantiate()
+  get_tree().get_root().add_child(keybindings)
